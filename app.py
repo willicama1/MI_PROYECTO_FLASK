b2172fdf15b1d9d2d682c8abda3299817b9054ab
willicama1 <wa.camachos@uea.edu.ec>
2025-08-23 16:33:45 -0500

    Organizado proyecto Flask con templates y static

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hola, Flask está funcionando ✅"
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/usuario/<nombre>")
 def usuario(nombre):
